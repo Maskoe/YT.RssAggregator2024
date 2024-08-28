a9b252cfc88bb4163ea0f7795bc702bf10ab0571
Mirco Benthien <Mirco@Benthien.dev>
2024-08-28 20:35:36 +0200

    Create GetAllFeedsEndpoint.cs

---
+++ RssAggregator/Features/GetAllFeedsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using RssAggregator.Db;
+
+namespace RssAggregator.Features;
+
+public class GetAllFeedsEndpoint : EndpointWithoutRequest<GetFeedsResponse>
+{
+    private readonly Context context;
+
+    public GetAllFeedsEndpoint(Context context)
+    {
+        this.context = context;
+    }
+
+    public override void Configure()
+    {
+        Get("api/feeds");
+    }
+
+    public override async Task<GetFeedsResponse> ExecuteAsync(CancellationToken ct)
+    {
+        var dtos = await context.Feeds
+            .Select(x => new FeedDto(x.Id, x.Name))
+            .ToListAsync();
+
+        return new GetFeedsResponse(dtos);
+    }
+}
+
+public record GetFeedsResponse(List<FeedDto> Feeds);
+
+public record FeedDto(Guid Id, string Name);
