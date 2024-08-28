cf43a940db7c57ca2f3f2400ef5edc0774d08f62
Mirco Benthien <Mirco@Benthien.dev>
2024-08-28 16:40:50 +0200

    ef core

---
+++ RssAggregator/Db/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RssAggregator.Db;
+
+public class Context : DbContext
+{
+
+    public DbSet<AppUser> Users { get; set; }
+
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
+public class AppUser
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; }
+}
--- RssAggregator/Program.cs
+++ RssAggregator/Program.cs
@@ -1,9 +1,16 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
+using RssAggregator.Db;
 
 var bld = WebApplication.CreateBuilder();
 bld.Services.AddFastEndpoints();
 bld.Services.SwaggerDocument();
+
+bld.Services.AddDbContextFactory<Context>(options =>
+{
+    options.UseNpgsql("User ID=postgres; Password=postgres; Database=ytRss; Server=localhost; Port=5432; Include Error Detail=true;");
+});
 
 var app = bld.Build();
 app.UseFastEndpoints();
